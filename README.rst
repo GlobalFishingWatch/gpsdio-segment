b4bd8677fdf2dbdfc5cdef8d21e9750c3d7cd124
Kevin Wurster <kevin@skytruth.org>
2015-06-03 17:44:48 -0400

    Packaging

---
+++ README.rst
@@ -0,0 +1,13 @@
+==============
+gpsdio-despoof
+==============
+
+.. image:: https://travis-ci.org/SkyTruth/gpsdio-despoof.svg?branch=master
+    :target: https://travis-ci.org/SkyTruth/gpsdio-despoof
+
+
+.. image:: https://coveralls.io/repos/SkyTruth/gpsdio-despoof/badge.svg?branch=master
+    :target: https://coveralls.io/r/SkyTruth/gpsdio-despoof
+
+
+Tools for despoofing AIS data and a CLI plugin for `gpsdio <https://github.com/skytruth/gpdsio/>`_.
---
+++ setup.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+
+
+"""
+Setup script for gpsdio-despoof
+"""
+
+
+import os
+from setuptools import find_packages
+from setuptools import setup
+
+
+with open('README.rst') as f:
+    readme = f.read().strip()
+
+
+with open('LICENSE.txt') as f:
+    license = f.read().strip()
+
+
+version = None
+author = None
+email = None
+source = None
+with open(os.path.join('gpsdio_despoof', '__init__.py')) as f:
+    for line in f:
+        if line.strip().startswith('__version__'):
+            version = line.split('=')[1].strip().replace('"', '').replace("'", '')
+        elif line.strip().startswith('__author__'):
+            author = line.split('=')[1].strip().replace('"', '').replace("'", '')
+        elif line.strip().startswith('__email__'):
+            email = line.split('=')[1].strip().replace('"', '').replace("'", '')
+        elif line.strip().startswith('__source__'):
+            source = line.split('=')[1].strip().replace('"', '').replace("'", '')
+        elif None not in (version, author, email, source):
+            break
+
+
+setup_args = dict(
+    author=author,
+    author_email=email,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Communications',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: GIS',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Utilities',
+    ],
+    description="A plugin for gpsdio to despoof data.",
+    entry_points='''
+        [gpsdio.gpsdio_plugins]
+        despoof=gpsdio_despoof.cli:despoof
+    ''',
+    extras_require={
+        'test': ['pytest', 'pytest-cov']
+    },
+    include_package_data=True,
+    install_requires=[
+        'gpsdio>=0.0.2',
+        'click>=0.3',
+        'pyproj'
+    ],
+    keywords='AIS GIS remote sensing raster',
+    license=license,
+    long_description=readme,
+    name='gpsdio-despoof',
+    packages=find_packages(),
+    url=source,
+    version=version,
+    zip_safe=True
+)
+
+
+setup(**setup_args)

848e3e2020dad34dadccb31a6a3db82ba4fdb378
Paul Woods <pwoods25443@gmail.com>
2016-08-03 18:36:12 -0400

    More test cases

---
+++ tests/data/expected/sql/367382540.sql
@@ -0,0 +1,11 @@
+SELECT mmsi, lat, lon,  
+  FORMAT_UTC_USEC(timestamp) as timestamp, 
+  shipname,
+  speed
+
+FROM TABLE_DATE_RANGE([pipeline_classify_logistic_661b.], 
+  TIMESTAMP('2015-01-01'), TIMESTAMP('2015-02-01'))
+
+where mmsi in (367382540)
+order by timestamp
+LIMIT 100

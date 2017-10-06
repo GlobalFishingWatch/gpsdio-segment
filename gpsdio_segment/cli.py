6a9108b4def05048db621c9bfdf7efdfdc6cfd2f
Paul Woods <pwoods25443@gmail.com>
2017-10-06 11:47:47 -0400

    Expose new params in cli

--- gpsdio_segment/cli.py
+++ gpsdio_segment/cli.py
@@ -14,6 +14,10 @@ from gpsdio_segment.core import Segmentizer
 from gpsdio_segment.core import DEFAULT_MAX_SPEED
 from gpsdio_segment.core import DEFAULT_MAX_HOURS
 from gpsdio_segment.core import DEFAULT_NOISE_DIST
+from gpsdio_segment.core import REPORTED_SPEED_MULTIPLIER
+from gpsdio_segment.core import MAX_SPEED_MULTIPLIER
+from gpsdio_segment.core import MAX_SPEED_EXPONENT
+
 
 
 @click.command()
@@ -31,21 +35,37 @@ from gpsdio_segment.core import DEFAULT_NOISE_DIST
 )
 @click.option(
     '--max-speed', type=click.FLOAT, default=DEFAULT_MAX_SPEED,
-    help="Units are knots.  Points with a speed above this value are always considered "
-         "discontinuous. (default: {})".format(DEFAULT_MAX_SPEED)
+    help="Maximum allowable speed over a long distance. Units are knots.  (default: {})".format(DEFAULT_MAX_SPEED)
 )
 @click.option(
     '--noise-dist', type=click.FLOAT, default=DEFAULT_NOISE_DIST,
-    help="DEPRECATED. Units are nautical miles.  Points within this distance are always considered "
-         "continuous.  Used to allow a certain amount of GPS noise. "
-         "(default: {})".format(DEFAULT_NOISE_DIST)
+    help="If a point is within this distance (nautical miles) to an existing segment "
+         "but does not match with any segment, then consider it to be noise and "
+         "emit it in a singleton segment. Set to 0 to disable this. Default: {} ".format(DEFAULT_NOISE_DIST)
+)
+@click.option(
+    '--reported-speed-multiplier', type=click.FLOAT, default=REPORTED_SPEED_MULTIPLIER,
+    help="multiplier applied to reported speed to determine the max allowable speed "
+         "Default: {} ".format(REPORTED_SPEED_MULTIPLIER)
+)
+@click.option(
+    '--max-speed-multiplier', type=click.FLOAT, default=MAX_SPEED_MULTIPLIER,
+    help="multiplier used to compute the max allowable speed "
+         "Default: {} ".format(MAX_SPEED_MULTIPLIER)
+)
+@click.option(
+    '--max-speed-exponent', type=click.FLOAT, default=MAX_SPEED_EXPONENT,
+    help="exponent used to compute max allowable speed (see usage in _segment_match_metric() "
+         "Default: {} ".format(MAX_SPEED_EXPONENT)
 )
 @click.option(
     '--segment-field', default='segment',
     help="Add the segment ID to this field when writing messages. (default: segment)"
 )
 @click.pass_context
-def segment(ctx, infile, outfile, mmsi, max_hours, max_speed, noise_dist, segment_field):
+def segment(ctx, infile, outfile, mmsi, max_hours, max_speed, noise_dist,
+            reported_speed_multiplier, max_speed_multiplier,
+            max_speed_exponent, segment_field):
 
     """
     Group AIS data into continuous segments.
@@ -61,7 +81,10 @@ def segment(ctx, infile, outfile, mmsi, max_hours, max_speed, noise_dist, segmen
         logger.debug("Beginning to segment")
         for t_idx, seg in enumerate(Segmentizer(
                 src, mmsi=mmsi, max_hours=max_hours,
-                max_speed=max_speed, noise_dist=noise_dist)):
+                max_speed=max_speed, noise_dist=noise_dist,
+                reported_speed_multiplier=reported_speed_multiplier,
+                max_speed_multiplier=max_speed_multiplier,
+                max_speed_exponent=max_speed_exponent)):
 
             logger.debug("Writing segment %s with %s messages and %s points",
                          seg.id, len(seg), len(seg.coords))

7715323720242e6f1a9555e5c57aa0ba9d589d55
Enrique Tuya <enriquetuya@gmail.com>
2019-03-11 15:06:30 -0300

    Fix version of the pyproj to 1.9.6 since 2.1.0 is not resolved correctly by pipe.

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ setup(
     install_requires=[
         'gpsdio==0.0.7',
         'click>=0.3',
-        'pyproj',
+        'pyproj==1.9.6',
         'newlinejson<1.0'
     ],
     keywords='AIS GIS remote sensing',

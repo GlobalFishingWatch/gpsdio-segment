205493d3dbdbd09d965e35faa53e95cfc70c3ef7
bitsofbits <tim@bitsofbits.com>
2019-11-08 06:24:21 -0700

    Finish moving existing stitcher functionality from existing notebook

--- gpsdio_segment/stitcher.py
+++ gpsdio_segment/stitcher.py
@@ -1,9 +1,7 @@
+from __future__ import division, print_function, absolute_import
 from collections import Counter
+from itertools import chain
 import math
-
-import numpy as np
-
-#TODO: remove numpy dependency
 
 from .discrepancy import DiscrepancyCalculator
 
@@ -31,11 +29,14 @@ class Stitcher(DiscrepancyCalculator):
     no_id_hour_penalty = 2.0 # be more strict for tracks with no id info joining them across long times
     speed_weight = 10.
     
+    def __init__(self, **kwargs):
+        for k in kwargs:
+            # TODO: when interface stable, fix keys as in core.py
+            self._update(k, kwargs)
     
     @staticmethod
     def add_track_ids(msgs, tracks):
         # Use the seg_id of the first seg in the track as the track id.
-        track_ids = np.array([None] * len(msgs))
         id_map = {}
         for track in tracks:
             track_id = track[0]['seg_id']
@@ -47,16 +48,21 @@ class Stitcher(DiscrepancyCalculator):
     @staticmethod
     def _extract_sig_part(msgs, inner_key, outer_key):
         counter = Counter()
-        try:
-            for m in msgs:
-                for x in m[outer_key]:
-                    counter.update({x[inner_key] : x['cnt']})
-            counted = counter.most_common(5)
-            total = sum(count for (name, count) in counted)
-        except:
-            print(inner_key, outer_key, m)
-            raise
+        for m in msgs:
+            for x in m[outer_key]:
+                counter.update({x[inner_key] : x['cnt']})
+        counted = counter.most_common(5)
+        total = sum(count for (name, count) in counted)
         return dict([(name, count / total) for (name, count) in counted]), total
+
+    @staticmethod
+    def _update_sig_part(counters, msg, inner_key, outer):
+        seg_id = msg['seg_id']
+        if seg_id not in counters:
+            counters[seg_id] = Counter()
+        counter = counters[seg_id]
+        for x in msg[outer_key]:
+            counter.update({x[inner_key] : x['cnt']})
     
     def _compute_signatures(self, msgs, segs):
         msgs_by_seg = {}
@@ -76,13 +82,20 @@ class Stitcher(DiscrepancyCalculator):
             # Compute eroded ends that ignore ends of segments in case there are a few bad points
             n = int(min(len(seg_msgs) *  self.max_overlap_fraction, self.max_overlap_points))
             eroded_ends[sid] = (seg_msgs[n]['timestamp'], seg_msgs[-(1 + n)]['timestamp'])
-            # Compute signature
-            # TODO: remove numpy dependency here
-            types = np.array([x['type'] for x in seg_msgs])
-            is_a = (types == 'AIS.1') | (types == 'AIS.2') | (types == 'AIS.3')
-            is_b = (types == 'AIS.18') | (types == 'AIS.19')
-            tp_count = (is_a | is_b).sum()
-            tp_type = {'is_A' : is_a.mean(), 'is_B' : is_b.mean()}
+            # TODO: rework so that we traverse messages at most twice (once above and once here)
+            # TODO: better yet traverse once
+            a_types = set(['AIS.1', 'AIS.2', 'AIS.3'])
+            b_types = set(['AIS.18', 'AIS.19'])
+            ab_types = a_types | b_types
+            a_cnt = b_cnt = tp_count = 0
+            for msg in seg_msgs:
+                a_cnt += msg['type'] in a_types
+                b_cnt += msg['type'] in b_types
+                tp_count += msg['type'] in ab_types
+            if tp_count == 0:
+                tp_type = {'is_A' : 0.5, 'is_B' : 0.5}
+            else:
+                tp_type = {'is_A' : a_cnt / tp_count, 'is_B' : b_cnt / tp_count}
             shipname, sn_count = self._extract_sig_part(seg_msgs, 'shipname', 'shipnames')
             callsign, cs_count = self._extract_sig_part(seg_msgs, 'callsign', 'callsigns')
             imo, imo_count = self._extract_sig_part(seg_msgs, 'imo', 'imos')
@@ -100,21 +113,43 @@ class Stitcher(DiscrepancyCalculator):
         sig1 = signatures[seg1['seg_id']]
         sig2 = signatures[seg2['seg_id']]
 
-        # TODO: sigmatch should treat / B signature differently than others since that should 
+        # TODO: match should treat / B signature differently than others since that should 
         # Almost always be present, and is 50/50 so is a much weaker signal. Primarily a negative
         # signal if A / B mismatch then it's likely different tracks, but if they do then who knows
         
-        # TODO: identical signatures where divided 50/50 currently have a metric of 50%
-        # Seems like it should be 100% but with a lower weight.
-        sigmatch = []
+        # TODO: leverage wts to return weight that influences how seriously we take this. Note note
+        #       about A/B above too.
+        match = []
         for a, b in zip(sig1, sig2):
             if len(a) and len(b):
                 x = 0
-                for k, v in a.items():
-                    if k in b:
-                        x += v * b[k]
-                sigmatch.append(x)
-        return 1 / np.mean([1 / (x + 1e-99) for x in sigmatch]) if sigmatch else self.min_sig_match, bool(sigmatch)
+                ta = 0 
+                tb = 0
+                for k in set(a) | set(b):
+                    x += a.get(k, 0) * b.get(k, 0)
+                    ta += a.get(k, 0) ** 2
+                    tb += b.get(k, 0) ** 2
+                wt = math.sqrt(ta * tb)
+                match.append((x, wt))
+        # print('>>>>')
+        # print(sig1)
+        # print(sig2)
+        # print(match)
+        if match:
+            # The idea here is that we want to consider 
+            #   (a) the worst match and
+            #   (b) the most specific match
+            # Minimizing x/wt minimizes a particular combination of
+            # specificity and badness, then we multiply by weight to
+            # get back the correct match value. There may be a cleaner
+            # approach.
+            x_wt, wt = min(((x / wt), wt) for (x, wt) in match)
+            sig_metric = x_wt * wt
+        else:
+            sig_metric = self.min_sig_match
+        # print(sig_metric)
+        # print('<<<<')
+        return sig_metric, bool(match)
     
     def create_tracks(self, msgs, segs):
         segs = self.filter_and_sort(segs)
@@ -130,30 +165,30 @@ class Stitcher(DiscrepancyCalculator):
                 seg_t0, seg_t1 = eroded_ends[seg['seg_id']]
                 tgt_t0, tgt_t1 = eroded_ends[tgt['seg_id']]
                 # TODO: rename to eroded_hours?
-                raw_hours = self.compute_ts_delta_hours(tgt_t1, seg_t0)
+                eroded_hours = self.compute_ts_delta_hours(tgt_t1, seg_t0)
                 dt1 = self.compute_ts_delta_hours(seg_t0, seg_t1)
                 dt2 = self.compute_ts_delta_hours(tgt_t0, tgt_t1)
                 max_overlap_hours = min(dt1 * self.max_overlap_fraction, 
                                         dt2 * self.max_overlap_fraction, self.max_overlap_hours)            
 
-                if raw_hours + max_overlap_hours <= 0:
+                if eroded_hours + max_overlap_hours <= 0:
                     continue 
-                hours = raw_hours + self.buffer_hours
 
                 laxity = (1 if (len(tracks) < self.penalty_tracks) else 
                         math.sqrt(2) / math.hypot(1, len(tracks)/self.penalty_tracks))
 
-                sig_metric, sigmatch = self.compute_signature_metric(signatures, seg, tgt)
+                sig_metric, had_match = self.compute_signature_metric(signatures, seg, tgt)
                 
-                hours_exp = 1.0 / self.base_hour_penalty if sigmatch else 1.0 / self.no_id_hour_penalty
+                hours_exp = 1.0 / self.base_hour_penalty if had_match else 1.0 / self.no_id_hour_penalty
         
                 
                 if laxity * sig_metric < self.min_sig_match:
+                    # print('failing match', laxity, sig_metric, self.min_sig_match)
                     continue
+                # print('succesful match', laxity, sig_metric, self.min_sig_match)
 
-                effective_hours =(hours if (hours < self.penalty_hours) else 
-                                  hours * (hours / self.penalty_hours) ** hours_exp)
 
+                # TODO: use eroded msgs
                 msg0 = {'timestamp' : tgt['last_timestamp'], 
                         'lat' : tgt['last_lat'], 'lon' : tgt['last_lon'],
                         'speed' : tgt['last_speed'], 'course' : tgt['last_course']}
@@ -165,14 +200,17 @@ class Stitcher(DiscrepancyCalculator):
                     msg0, msg1 = msg1, msg0
 
 
-                hours = self.compute_msg_delta_hours(msg0, msg1)
+                hours = math.hypot(self.compute_msg_delta_hours(msg0, msg1), self.buffer_hours)
+                effective_hours =(hours if (hours < self.penalty_hours) else 
+                                    hours * (hours / self.penalty_hours) ** hours_exp)
                 discrepancy = self.compute_discrepancy(msg0, msg1)
 
                 speed = discrepancy / effective_hours # TODO: use segmentizer hours?
                 if speed > self.max_average_knots * laxity:
+                    # print('failing speed match', speed, laxity, effective_hours)
                     continue
 
-                speed_metric = np.exp(-(speed / self.speed_0) ** 2) / hours    
+                speed_metric = math.exp(-(speed / self.speed_0) ** 2) / hours    
                 metric = sig_metric + self.speed_weight * speed_metric
                 if metric > best_metric:
 
@@ -197,5 +235,7 @@ class Stitcher(DiscrepancyCalculator):
                 best_track.append(seg)
             elif seg['message_count'] >= self.min_seed_size:
                 tracks.append([seg])
+            # print()
+            # print()
                 
         return tracks

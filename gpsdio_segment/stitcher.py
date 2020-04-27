989a7e663efa7ae364f0f98917f5f694cec7b6aa
bitsofbits <tim@bitsofbits.com>
2020-04-27 06:09:46 -0700

    Fix typo in setting debug level

--- gpsdio_segment/stitcher.py
+++ gpsdio_segment/stitcher.py
@@ -8,7 +8,7 @@ from .discrepancy import DiscrepancyCalculator
 
 logging.basicConfig()
 logger = logging.getLogger(__file__)
-logger.setLevel(logging.debug)
+logger.setLevel(logging.DEBUG)
 
 Track = namedtuple('Track', ['id', 'prefix', 'segments', 'count', 'decayed_count', 'is_active',
                              'signature'])
